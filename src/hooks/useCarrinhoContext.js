8608df1ea79e03f9016e98f0dea9c410cc455b85
Sponchiatto <rafael_sponchiatto@hotmail.com>
2024-06-01 18:16:02 -0300

    Update useReducer
    Utilizando o useReducer para treinar e organizar o código

--- src/hooks/useCarrinhoContext.js
+++ src/hooks/useCarrinhoContext.js
@@ -1,82 +1,50 @@
 import { useContext, useEffect, useMemo } from "react";
 import { CarrinhoContext } from "../context/CarrinhoContext";
+import {
+  ADD_PRODUTO,
+  REMOVE_PRODUTO,
+  UPDATE_QUANTIDADE,
+} from "../reducers/carrinhoReducer";
+
+const addProdutoAction = (novoProduto) => ({
+  type: ADD_PRODUTO,
+  payload: novoProduto,
+});
+
+const removerProdutoAction = (produtoId) => ({
+  type: REMOVE_PRODUTO,
+  payload: produtoId,
+});
+
+const updateQuantidadeAction = (produtoId, quantidade) => ({
+  type: UPDATE_QUANTIDADE,
+  payload: { produtoId, quantidade },
+});
 
 export const useCarrinhoContext = () => {
-  const {
-    carrinho,
-    setCarrinho,
-    quantidade,
-    setQuantidade,
-    valorTotal,
-    setValorTotal,
-  } = useContext(CarrinhoContext);
-
-  function mudarQuantidade(id, quantidade) {
-    return carrinho.map((itemDoCarrinho) => {
-      if (itemDoCarrinho.id === id) itemDoCarrinho.quantidade += quantidade;
-      return itemDoCarrinho;
-    });
-  }
+  const { carrinho, dispatch, quantidade, valorTotal } =
+    useContext(CarrinhoContext);
 
   function adicionarProduto(novoProduto) {
-    const temOProduto = carrinho.some(
-      (itemDoCarrinho) => itemDoCarrinho.id === novoProduto.id
-    );
-    if (!temOProduto) {
-      novoProduto.quantidade = 1;
-      return setCarrinho((carrinhoAnterior) => [
-        ...carrinhoAnterior,
-        novoProduto,
-      ]);
-    }
-    const carrinhoAtualizado = mudarQuantidade(novoProduto.id, 1);
-
-    setCarrinho([...carrinhoAtualizado]);
+    dispatch(addProdutoAction(novoProduto));
   }
 
   function removerProduto(id) {
-    // encontra o produto em questão
-    const produto = carrinho.find((itemDoCarrinho) => itemDoCarrinho.id === id);
-    // verifica se a quantidade é igual a um. Isso significa que este é o último
-    // produto do tipo no carrinho
-    const ehOUltimo = produto.quantidade === 1;
-    // Com o if faz a verificação do último produto do tipo no carrinho e atualiza o estado do carrinho
-    if (ehOUltimo) {
-      return setCarrinho((carrinhoAnterior) =>
-        carrinhoAnterior.filter((itemDoCarrinho) => itemDoCarrinho.id !== id)
-      );
+    const produto = carrinho.find((item) => item.id == id);
+
+    if (produto && produto.quantidade > 1) {
+      dispatch(updateQuantidadeAction(id, produto.quantidade - 1));
+    } else {
+      dispatch(removerProdutoAction(id));
     }
-
-    const carrinhoAtualizado = mudarQuantidade(id, -1);
-
-    setCarrinho([...carrinhoAtualizado]);
   }
 
   function removerProdutoCarrinho(id) {
-    const produto = carrinho.filter((itemDoCarrinho) => {
-      return itemDoCarrinho.id !== id;
-    });
-    setCarrinho(produto);
+    dispatch(removerProdutoAction(id));
   }
-
-  const { totalTemp, quantidadeTemp } = useMemo(() => {
-    return carrinho.reduce(
-      (acumulador, produto) => ({
-        quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
-        totalTemp: acumulador.totalTemp + produto.quantidade * produto.preco,
-      }),
-      { quantidadeTemp: 0, totalTemp: 0 }
-    );
-  }, [carrinho]);
-
-  useEffect(() => {
-    setQuantidade(quantidadeTemp);
-    setValorTotal(totalTemp);
-  });
 
   return {
     carrinho,
-    setCarrinho,
     adicionarProduto,
     removerProduto,
     removerProdutoCarrinho,
